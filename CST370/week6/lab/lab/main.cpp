33d1285c8fa944cf8668da11c8e6dfc84085e9e2
Matthew Bozelka <mattbozelka.dev@gmail.com>
2016-02-15 12:22:32 -0800

    adding week 6 assignemnts

---
+++ CST370/week6/lab/lab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab
+//
+//  Created by Matthew Bozelka on 2/12/16.
+//  Copyright © 2016 Matthew Bozelka. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
